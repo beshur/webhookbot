41b353fa3cf3b4495162b10f63364c92ba715fbd
Alex Buznik <shu@buznik.net>
2018-05-15 14:28:04 +0300

    Update fb page access token

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ function callSendAPI(sender_psid, response) {
   // Send the HTTP request to the Messenger Platform
   request({
     "uri": "https://graph.facebook.com/v2.6/me/messages",
-    "qs": { "access_token": config.FB_APP_SECRET },
+    "qs": { "access_token": config.FB_PAGE_ACCESS_TOKEN },
     "method": "POST",
     "json": request_body
   }, (err, res, body) => {
