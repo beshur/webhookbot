9f07108d5af63173d8bf1abf95e73337f751694d
Alex Buznik <shu@buznik.net>
2018-06-09 23:13:58 +0300

    Analytics class rewrite, #6

--- src/Analytics.js
+++ src/Analytics.js
@@ -2,72 +2,63 @@
 
 const request = require('request');
 const _ = require('underscore');
-const collectEndpoint = 'https://www.google-analytics.com/collect';
 const WebhookTypes = require('./WebhookTypes');
 
-function Analytics(GA_ID, webhookType) {
-  this.GA_ID = GA_ID;
-  this.LOG = 'Analytics';
+class Analytics {
+  constructor(GA_ID, webhookType) {
+    this.GA_ID = GA_ID;
+    this.LOG = 'Analytics';
+    this.collectEndpoint = 'https://www.google-analytics.com/collect';
+    this.type;
+    this.typeLabel;
 
-  this.setupAnalytics = (webhookType) => {
+    this.setupAnalytics(webhookType);
+  }
+
+  setupAnalytics(webhookType) {
     this.type = webhookType;
     this.typeLabel = _.invert(WebhookTypes)[webhookType];
   }
 
-  this.setupAnalytics(webhookType);
-
-  this.eventTpl = {
-    v: 1,
-    t: 'event',
-    tid: this.GA_ID,
-    cid: '',
-    ec: 'webhook',
-    ea: ''
+  get eventTpl() {
+    return {
+      v: 1,
+      t: 'event',
+      tid: this.GA_ID,
+      cid: '',
+      ec: 'webhook',
+      ea: ''
+    }
   }
 
-  this.trackNewWebhook = (userId) => {
-    const event = _.defaults({
+  getEventObj(eventAction, userId) {
+    return _.defaults({
       cid: userId,
-      ea: 'created',
+      ea: eventAction,
       el: this.typeLabel
     }, this.eventTpl);
+  };
 
-    return this.collect(event);
+  trackNewWebhook(userId) {
+    return this.collect(this.getEventObj('created', userId));
   }
 
-  this.trackUpdateWebhook = (userId) => {
-    const event = _.defaults({
-      cid: userId,
-      ea: 'updated',
-      el: this.typeLabel
-    }, this.eventTpl);
-
-    return this.collect(event);
+  trackUpdateWebhook(userId) {
+    return this.collect(this.getEventObj('updated', userId));
   }
 
-  this.trackDeleteWebhook = (userId) => {
-    const event = _.defaults({
-      cid: userId,
-      ea: 'deleted',
-      el: this.typeLabel
-    }, this.eventTpl);
-
-    return this.collect(event);
+  trackDeleteWebhook(userId) {
+    return this.collect(this.getEventObj('deleted', userId));
   }
 
-  this.trackWebhookHit = (userId) => {
-    const event = _.defaults({
-      cid: userId,
-      ea: 'hit',
-      el: this.typeLabel
-    }, this.eventTpl);
-    return this.collect(event);
+  trackWebhookHit(userId) {
+    return this.collect(this.getEventObj('hit', userId));
   }
 
-  this.collect = function(eventData) {
+  collect(eventData) {
     return new Promise((resolve, reject) => {
       request({
-        'uri': collectEndpoint,
+        'uri': this.collectEndpoint,
         'qs': eventData,
         'method': 'POST'
       }, (err, res, body) => {
