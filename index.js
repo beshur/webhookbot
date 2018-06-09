e52df8aadc0cf9ef7ba480f97eda65b46b06cdaa
Alex Buznik <shu@buznik.net>
2018-06-10 00:41:31 +0300

    Cleanup of some unused code, #6

--- index.js
+++ index.js
@@ -118,44 +118,6 @@ app.post('/webhook', (req, res) => {
 });
 
 // Creates the endpoint for our Telegram webhook 
-app.post('/webhook', (req, res) => {  
- 
-  let body = req.body;
-  console.log(body);
-  // Checks this is an event from a page subscription
-  if (body.object === 'page') {
-
-    // Iterates over each entry - there may be multiple if batched
-    body.entry.forEach(function(entry) {
-
-      // Gets the message. entry.messaging is an array, but 
-      // will only ever contain one message, so we get index 0
-      let webhook_event = entry.messaging[0];
-      console.log("webhook event", webhook_event);
-
-      // Get the sender PSID
-      let sender_psid = webhook_event.sender.id;
-      console.log('Sender PSID: ' + sender_psid);
-
-      if (webhook_event.message) {
-        fbMesControllerInstance.handleMessage(sender_psid, webhook_event.message);        
-      } else if (webhook_event.postback) {
-        fbMesControllerInstance.handlePostback(sender_psid, webhook_event.postback);
-      }
-
-    });
-
-    // Returns a '200 OK' response to all requests
-    res.status(200).send('EVENT_RECEIVED');
-  } else {
-    // Returns a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404);
-  }
-
-});
-
-
-// Creates the endpoint for our Telegram webhook 
 app.post('/tg/:token', (req, res) => {  
  
   let body = req.body;
